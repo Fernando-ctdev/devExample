be493e3436cd07c96b318e1fa9cfc2d8548308ef
Maicon Fernando <138537994+Fernando-ctdev@users.noreply.github.com>
2025-02-13 18:58:04 -0300

    Movendo utils/neon.ts para api/config/db.ts

--- api/technologies.ts
+++ api/technologies.ts
@@ -1,5 +1,5 @@
 import type { VercelRequest, VercelResponse } from '@vercel/node';
-import pool from '../utils/neon';
+import pool from './config/db';
 
 export default async function handler(req: VercelRequest, res: VercelResponse) {
   if (req.method === 'GET') {
