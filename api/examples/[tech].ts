0e587760964c1da9ee0d289ed52f4e95bbe0deb2
Maicon Fernando <138537994+Fernando-ctdev@users.noreply.github.com>
2025-02-13 18:08:21 -0300

    mudança de db para neon

--- api/examples/[tech].ts
+++ api/examples/[tech].ts
@@ -1,5 +1,5 @@
 import type { VercelRequest, VercelResponse } from '@vercel/node';
-import pool from '../lib/neon.js';
+import pool from '../lib/neon';
 
 export default async function handler(req: VercelRequest, res: VercelResponse) {
   if (req.method === 'GET') {
