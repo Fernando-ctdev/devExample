2f66d48a715fc67fc4b0d53a590f5ce6358b9b31
Maicon Fernando <138537994+Fernando-ctdev@users.noreply.github.com>
2025-02-13 18:41:15 -0300

    asgawsefbg

--- api/topics/index.ts
+++ api/topics/index.ts
@@ -1,5 +1,5 @@
 import type { VercelRequest, VercelResponse } from '@vercel/node';
-import pool from '../../../utils/neon';
+import pool from '../utils/neon';
 
 export default async function handler(req: VercelRequest, res: VercelResponse) {
   if (req.method === 'POST') {
